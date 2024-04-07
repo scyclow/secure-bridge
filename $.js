89fcbb0836fda7f0ef07d171c40aaf26b0592983
Steve Pikelny <spike716@gmail.com>
2024-04-07 19:15:10 -0400

    init

---
+++ $.js
@@ -0,0 +1,195 @@
+
+export const $ = (elem, prop, value) => elem.style[prop] = value
+
+window.$ = $
+$.cls = (selector, elem=document) => Array.isArray(elem)
+  ? elem.map(e => $.cls(e, selector)).flat()
+  : Array.from(elem.getElementsByClassName(selector))
+
+$.id = (selector, elem=document) => Array.isArray(elem)
+  ? elem.find(e => $.id(e, selector))
+  : elem.getElementById(selector)
+
+
+$.render = (e, children) => {
+  if (!children) return
+  else if (typeof children === 'string') e.innerHTML = children
+  else if (Array.isArray(children)) {
+    if (typeof children[0] === 'string') {
+      children.forEach(child => {
+        e.innerHTML += (
+          typeof child === 'string' ? child : child.outerHTML
+        )
+      })
+    } else {
+      e.append(...children.flat())
+    }
+  }
+  else {
+    e.append(children)
+  }
+}
+
+
+$.create = elType => (children, attrs={}) => {
+  const e = document.createElement(elType)
+  $.render(e, children)
+
+  Object.keys(attrs).forEach(a => {
+    e.setAttribute(a, attrs[a])
+  })
+
+  return e
+}
+
+$.a = $.create('a')
+$.li = $.create('li')
+$.div = $.create('div')
+$.span = $.create('span')
+$.main = $.create('main')
+$.section = $.create('section')
+
+
+const $html = document.getElementsByTagName('html')[0]
+
+export let queryParams
+
+try {
+  queryParams = window.location.search
+    ? window.location.search.replace('?', '').split('&').reduce((params, i) => {
+        const [k, v] = i.split('=')
+        params[k] = v
+        return params
+      }, {})
+    : {}
+} catch (e) {
+  queryParams = {}
+}
+
+
+
+
+const addMetaTag = (args) => {
+  const meta = document.createElement('meta')
+  Object.keys(args).forEach(arg => {
+    meta[arg] = args[arg]
+  })
+
+  document.head.appendChild(meta)
+}
+
+const addThumbnail = (fill) => {
+  const existing = document.getElementById('favicon')
+  if (existing) document.head.removeChild(existing)
+  const link = document.createElement('link')
+  link.href = `data:image/svg+xml;base64,${btoa(
+    `<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"><rect x="0" y="0" width="1" height="1" fill="${fill}"></rect></svg>`
+  )}`
+  link.rel = 'shortcut icon'
+  link.type = 'image/svg+xml'
+  link.id = 'favicon'
+  document.head.appendChild(link)
+}
+
+function css(style) {
+  const s = document.createElement('style')
+  s.innerHTML = style
+  document.head.appendChild(s)
+}
+
+
+function setMetadata() {
+  $html.translate = false
+  $html.lang = 'en'
+  $html.className = 'notranslate'
+
+  addMetaTag({ name: 'google', content: 'notranslate' })
+}
+
+setMetadata()
+
+export const ls = {
+  get(key) {
+    try {
+      return window.localStorage && window.localStorage.getItem && JSON.parse(window.localStorage.getItem(key))
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  set(key, value) {
+    try {
+      return window.localStorage.setItem(key, value)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+window.ls = ls
+
+export const createComponent = (tag, templateStr, initialState, onInit, onRender) => {
+  class ReactStyleComponent extends HTMLElement {
+    constructor() {
+      super();
+
+      // Initialize component state (similar to React's state)
+      this.state = Object.assign({}, initialState)
+      this.oldState = this.state
+      this.events = {}
+
+      // Create a shadow DOM and attach it to the element
+      const shadowRoot = this.attachShadow({ mode: 'open' })
+
+      // Define a template for the web component
+      const template = document.createElement('template');
+      template.innerHTML = templateStr;
+
+      // Clone the template content and append it to the shadow DOM
+      shadowRoot.appendChild(template.content.cloneNode(true));
+
+      const qs = shadowRoot.querySelector.bind(shadowRoot)
+      this.$ = selector => {
+        const e = qs(selector)
+        if (selector[0] === '.') {
+          return e ? Array.from(e) : []
+        } else {
+          return e
+        }
+      }
+
+      this.onRender = onRender
+      onInit(this)
+    }
+
+    veiwState() {
+      return this.state
+    }
+
+    // Define a method to set the component state
+    setState(newState) {
+      this.oldState = this.state
+      this.state = { ...this.state, ...newState };
+
+      if (deepEquals(this.state, this.oldState)) return
+      this.render()
+    }
+
+    // Define a method to render the component
+    render() {
+      this.onRender(this)
+    }
+
+    // Called when the element is connected to the DOM
+    connectedCallback() {
+      this.render();
+    }
+
+    registerEventHandler(event, fn) {
+      if (this.events[event]) this.events[event].push(fn)
+      else this.events[event] = [fn]
+    }
+  }
+
+  customElements.define(tag, ReactStyleComponent)
+}
+
---
+++ eth.js
@@ -0,0 +1,187 @@
+import {} from './min.ethers.js'
+
+export const bnToN = bn => Number(bn.toString())
+export const ethVal = n => Number(ethers.utils.formatEther(n))
+export const truncateAddr = (addr, len=13) => {
+  const padding = Math.floor((len - 5)/2)
+  return addr.slice(0, 2+padding) + '...' + addr.slice(-padding)
+}
+export const toETH = amt => ethers.utils.parseEther(String(amt))
+export const fromWei = amt => bnToN(amt)/1e18
+export const ethValue = amt => ({ value: toETH(amt) })
+export const txValue = amt => ({ value: toETH(amt) })
+export const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+
+
+export function isENS(ens) {
+  return ens.slice(-4) === '.eth'
+}
+
+window.toETH = toETH
+window.bnToN = bnToN
+window.fromWei = fromWei
+
+
+
+
+export class Web3Provider {
+  onConnectCbs = []
+
+  constructor() {
+    if (window.ethereum) {
+      try {
+        this.provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+        this.isEthBrowser = true
+
+        let currentAccount
+
+        this.provider.listAccounts().then(accounts => currentAccount = accounts[0])
+
+        setRunInterval(async () => {
+          const [connectedAccount, ...accounts] = await this.provider.listAccounts()
+          if (currentAccount !== connectedAccount) {
+            currentAccount = connectedAccount
+            this.connect()
+          }
+        }, 500)
+      } catch (e) {
+        console.error(e)
+      }
+
+    } else {
+      console.log('no Web3 detected')
+      this.isEthBrowser = false
+    }
+  }
+
+  onConnect(cb, errorCb) {
+    this.onConnectCbs.push(cb)
+    this.isConnected()
+      .then(addr => {
+        console.log(`New connection: ${addr}`)
+        if (addr) {
+          cb(addr)
+        }
+      })
+      .catch(errorCb)
+  }
+
+  connect() {
+    this.onConnectCbs.forEach(async cb => cb(await this.isConnected()))
+  }
+
+  get signer() {
+    return this.provider.getSigner()
+  }
+
+  async isConnected() {
+    if (!this.isEthBrowser) return false
+
+    try {
+      return await this.signer.getAddress()
+    } catch (e) {
+      return false
+    }
+  }
+
+  isWeb3() {
+    return !!window.ethereum
+  }
+
+  rawContract(contractAddr, abi) {
+    return new ethers.Contract(contractAddr, abi, this.provider)
+  }
+
+  async contract(contractAddr, abi) {
+    const signer = await this.isConnected()
+    if (signer) {
+      return (new ethers.Contract(contractAddr, abi, this.provider)).connect(this.signer)
+    }
+  }
+
+  isAddress(addr) {
+    return ethers.utils.isAddress(addr)
+  }
+
+
+  async getENS(addr) {
+    return this.provider.lookupAddress(addr)
+  }
+
+  async getTransactionCount(addr) {
+    return this.provider.getTransactionCount(addr)
+  }
+
+
+  async formatAddr(addr, truncate=true, nameLength=19) {
+    try {
+      const ens = await this.getENS(addr)
+      if (ens.slice(-4) === '.eth') {
+        return ens.length > nameLength
+          ? ens.slice(0, nameLength-3) + '...'
+          : ens
+      } else {
+        return truncate ? truncateAddr(addr, nameLength) : addr
+      }
+    } catch (e) {
+      return truncate ? truncateAddr(addr, nameLength) : addr
+    }
+  }
+
+  async getETHBalance(addr) {
+    return (await this.provider.getBalance(addr))
+  }
+
+  async getNetwork() {
+    const network = await this.provider.getNetwork()
+    const hasName = network.name && network.name !== 'unknown'
+    const { chainId } = network
+
+    let name
+    if (network.chainId === 1) {
+      name = 'mainnet'
+    } else if (network.chainId === 31337) {
+      name = 'local'
+    } else if (network.chainId === 11155111) {
+      name = 'sepolia'
+    } else if (hasName) {
+      name = network.name
+    } else {
+      name = network.chainId
+    }
+
+
+    const etherscanPrefix = name === 'goerli' ? 'goerli.' : ''
+
+
+    return { name, chainId, hasName, network, etherscanPrefix }
+  }
+
+  async contractEvents(contract, event, filterArgs) {
+    const filter = contract.filters[event](...filterArgs)
+    return (await contract.queryFilter(filter)).map(e => ({
+      ...e.args,
+      blockNumber: e.blockNumber,
+      txHash: e.transactionHash
+    }))
+  }
+
+  async setContractInfo(contractInfo) {
+    this.contractInfo = contractInfo
+  }
+  async getContracts() {
+    const n = await this.getNetwork()
+    const networkName = (await this.getNetwork()).name
+    const contractInfo = this.contractInfo
+
+    const output = {}
+    for (let c of Object.keys(contractInfo)) {
+      output[c] = await this.contract(contractInfo[c].addr[networkName], contractInfo[c].abi)
+    }
+    return output
+  }
+}
+
+
+
+export const provider = new Web3Provider()
